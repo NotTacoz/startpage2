09da68a164b7c4991765ccd24edc11808ada2eae
NotTacoz <tacozpvp@gmail.com>
2023-09-12 10:31:28 +0800

    initial commit

---
+++ index.ts
@@ -0,0 +1,10 @@
+console.log("Hello via Bun!");
+
+const server = Bun.serve({
+  port: 3000,
+  fetch(request) {
+    return new Response("Welcome to Bun!");
+  },
+});
+
+console.log(`Listening on localhost:${server.port}`);
