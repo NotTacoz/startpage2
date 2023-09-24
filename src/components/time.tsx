046709479640477db71fe558ec546abdbd9e4a6a
NotTacoz <tacozpvp@gmail.com>
2023-09-24 21:03:09 +0800

    time updates immediately

--- src/components/time.tsx
+++ src/components/time.tsx
@@ -5,6 +5,7 @@ export default function Time() {
     const [unix, setUnix] = useState<any>()
 
     useEffect(() => {
+        setUnix(Date.now()) // runs immediately
         const interval = setInterval(() => {
             setUnix(Date.now())
         }, 1000)
@@ -13,7 +14,7 @@ export default function Time() {
 
     return (
         <div>
-            <p>{new Date(unix).toLocaleDateString()}</p>
+            <p>{new Date(unix).toDateString()}</p>
             <p>{new Date(unix).toLocaleTimeString()}</p>
         </div>
     )
